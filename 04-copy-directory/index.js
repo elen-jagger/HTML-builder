9361f2a6a83f4bbfc3d0c33bc937ab6ddb3458be
Elena Leusik <elena.leusik@gmail.com>
2023-04-02 18:30:39 +0200

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('node:fs');
+const path = require('node:path');
 
+const sourseDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+async function copyDir() {
+  await fs.promises.mkdir(destDir, { recursive: true });
+  const sourseFiles = await fs.promises.readdir(sourseDir);
+
+  for (let file of sourseFiles) {
+    const soursePath = path.join(sourseDir, file);
+    const destPath = path.join(destDir, file);
+
+    fs.copyFile(soursePath, destPath, () => {});
+    console.log(file);
+  }
+}
+
+copyDir();
