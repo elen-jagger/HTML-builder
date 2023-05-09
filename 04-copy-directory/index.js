15f3551f21589a230476bba826085a4a2dccaae3
Elena Leusik <elena.leusik@gmail.com>
2023-05-09 12:55:31 +0200

    fix: add clear folder before copy option in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,11 @@ const sourseDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
 async function copyDir() {
+  const dirStat = await fs.promises.stat(destDir);
+  if (dirStat) {
+    await fs.promises.rm(destDir, { recursive: true });
+  }
+
   await fs.promises.mkdir(destDir, { recursive: true });
   const sourseFiles = await fs.promises.readdir(sourseDir);
 
@@ -13,7 +18,6 @@ async function copyDir() {
     const destPath = path.join(destDir, file);
 
     fs.copyFile(soursePath, destPath, () => {});
-    console.log(file);
   }
 }
 
