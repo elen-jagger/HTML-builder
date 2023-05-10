a7a7620525a036f73a6c20234feb27b3a4167650
Elena Leusik <elena.leusik@gmail.com>
2023-05-10 05:41:12 +0200

    fix: fit error when folder doesn't exist in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,9 +4,15 @@ const path = require('node:path');
 const sourseDir = path.join(__dirname, 'files');
 const destDir = path.join(__dirname, 'files-copy');
 
+async function checkExiststance(dirPath) {
+  return fs.promises.access(dirPath, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
+
 async function copyDir() {
-  const dirStat = await fs.promises.stat(destDir);
-  if (dirStat) {
+  const dirExist = await checkExiststance(destDir);
+  if (dirExist) {
     await fs.promises.rm(destDir, { recursive: true });
   }
 
